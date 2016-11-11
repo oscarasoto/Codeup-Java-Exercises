7e6e06c9622924d1c0f8a58d55c6bf701581d3f2
Oscar Soto <oscarasotoa@gmail.com>
2016-11-11 11:46:14 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by oscarsoto on 11/11/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+        System.out.print("Hello Mammonth!!");
+    }
+}
