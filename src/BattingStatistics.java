3f646a4ad4238724acd6ca207ace27eb0ae449d1
Oscar Soto <oscarasotoa@gmail.com>
2016-11-24 12:41:02 -0600

    Refactor exercise using bi-dimensional Arrays

--- src/BattingStatistics.java
+++ src/BattingStatistics.java
@@ -1,6 +1,9 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
+ * Calculate the batting average and slugging percentage for one or more baseball/softball players.
+ *
  * @author Oscar Soto on 11/21/16.
  *         There is no reasonable excuse for doing anything less than your best.
  *         - Martin, Robert C.
@@ -9,78 +12,120 @@ public class BattingStatistics {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String anotherBatter;
-        int [] atBats;
-        String battingAverage;
-        double sluggingPercentage;
+        int [][] atBats;
 
-// Calculate the batting average and slugging percentage for one or more baseball/softball players.
         System.out.println("Welcome to Batting Statistics");
 
-        do {
+        atBats = new int[getNumberOfBatters()][getNumberOfAtBats()];
 
+        for (int batterIndex = 0; batterIndex < atBats.length; batterIndex++){
 
-            atBats = new int[getNumberOfAtBats()];
-
-            for (int i = 0; i < atBats.length; i++) {
-                atBats[i] = getNumberOfBases(i);
+            for (int atBatIndex = 0; atBatIndex < atBats[batterIndex].length; atBatIndex++) {
+                atBats[batterIndex][atBatIndex] = getNumberOfBases(batterIndex, atBatIndex);
             }
 
-            battingAverage = calculateBattingAverage(atBats);
-            sluggingPercentage = calculateSluggingPercentage(atBats);
+        }
 
-            System.out.println("Batting average is: " + battingAverage); // This hide the format in the method.
-            System.out.printf("Slugging percentage is %.3f%n4", sluggingPercentage); // Format the value received by the method.
+        for (int batterIndex = 0; batterIndex < atBats.length; batterIndex++){
+            System.out.println("Batter " + (batterIndex+1) + " average: " + calculateBattingAverage(atBats[batterIndex], atBats.length)
+                    + "    slugging percentage: " + calculateSluggingPercentage(atBats[batterIndex], atBats.length));
 
-
-            System.out.println("Another batter yes/no");
-            anotherBatter = sc.nextLine();
-
-        } while (anotherBatter.equalsIgnoreCase("yes"));
+        }
 
 
+        sc.close();
 
-//    Slugging percentage is the total number of bases earned divided by the total number of at-bats.
+    }
+
+    private static int getNumberOfBatters() {
+        Scanner sc = new Scanner((System.in));
+        int numberOfBatters;
+        System.out.println("Please enter the number of batters");
+
+        try {
+            numberOfBatters = sc.nextInt();
+            if (numberOfBatters > 0){
+                return numberOfBatters;
+            } else {
+                throw new IllegalArgumentException("Please enter positive integers only");
+            }
+
+        } catch (InputMismatchException | IllegalArgumentException e) {
+            return getNumberOfBatters();
+
+        }
 
     }
 
     private static int getNumberOfAtBats() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter the number of at bats");
-        return sc.nextInt();
+        int numberOfAtBats;
+        System.out.println("Please enter the number of at bats, (only positive integers)");
+
+        try {
+            numberOfAtBats = sc.nextInt();
+            if (numberOfAtBats > 0){
+                return numberOfAtBats;
+            } else {
+                throw new IllegalArgumentException("Please enter positive integers only");
+            }
+
+
+        } catch (InputMismatchException | IllegalArgumentException e) {
+            return getNumberOfAtBats();
+
+        }
+
     }
 
-    private static int getNumberOfBases(int atBatIndex) {
+    private static int getNumberOfBases(int batterIndex, int atBatIndex) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter the number of bases for at Bat " + (atBatIndex+1));
-        return sc.nextInt();
+        int numberOfBases;
+        System.out.println("Please enter the number of bases for batter " + (batterIndex+1) +
+                " for at Bat " + (atBatIndex+1) + " (only positive integers from 0 to 4)");
+
+        try {
+            numberOfBases = sc.nextInt();
+            if (numberOfBases >= 0 && numberOfBases <= 4) {
+                return numberOfBases;
+            } else {
+                throw new IllegalArgumentException("Please enter positive integers only");
+            }
+
+        } catch (InputMismatchException | IllegalArgumentException e) {
+            return getNumberOfBases(batterIndex, atBatIndex);
+
+        }
     }
 
-    private static String calculateBattingAverage(int[] atBats){
-        double totalNumberOfGoodAtBats = 0;
-        double average;
+    private static String calculateBattingAverage(int[] batterData, int totalAtBats){
+        double countOfHits = 0;
+        double battingAverage;
 
-        for (int atBat : atBats) {
-            if (atBat > 0) {
-                totalNumberOfGoodAtBats++;
+        for (int data : batterData){
+            if (data > 0) {
+                countOfHits++;
+                System.out.println(data);
             }
         }
+        System.out.println(countOfHits);
+        System.out.println(totalAtBats);
+        battingAverage = countOfHits / totalAtBats;
 
-        average = totalNumberOfGoodAtBats / atBats.length;
-        return String.format("%.3f", average);
+        return String.format("%.3f",battingAverage);
     }
 
-    private static double calculateSluggingPercentage(int[] atBats){
-        double numberOfBases = 0;
-        double average;
-        for (int atBat : atBats) {
-            numberOfBases += atBat;
+    private static String calculateSluggingPercentage(int[] batterData, int totalAtBats){
+        double sumOfBases = 0;
+        double sluggingPercentage;
+
+        for (int numberOfBases : batterData){
+            sumOfBases += numberOfBases;
         }
+        sluggingPercentage = sumOfBases / totalAtBats;
 
-        System.out.println(numberOfBases);
-        System.out.println(atBats.length);
-        average = numberOfBases / atBats.length;
-        return average;
+        return String.format("%.3f",sluggingPercentage);
     }
+
 
 }
